f4cb7b8d9a6ea17b07f912d379c321aaaa346dc1
Eric Strong <ericstrong89@gmail.com>
2017-09-25 10:25:19 +0100

    lab3 done

--- app/src/main/java/com/example/soc7/labapp2/MainActivity.java
+++ app/src/main/java/com/example/soc7/labapp2/MainActivity.java
@@ -4,19 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    public Button bt1 = null;
+    private Button btn1 = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
+        btn1 = (Button) findViewById(R.id.clickMeBtn);
+        //when button clicked
 
-    }
+        btn1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "OUCH!!! You clicked me!!",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }//end on Create
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
